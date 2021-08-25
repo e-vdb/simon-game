4832130f52d4ab66ba54461cb5271d9d5815d98c
Emeline Van der Beken <82372483+e-vdb@users.noreply.github.com>
2021-08-25 13:45:16 +0200

    Update simon.py

--- simon.py
+++ simon.py
@@ -8,7 +8,8 @@ seqColors=[]
 lgSeq=1
 can_play=False
 player_prop=[]
-messages=["Click on Play to launch the sequence","Simon sequence","Reproduce the sequence"]
+instructions=["Click on Play to launch the sequence","Simon sequence","Reproduce the sequence"]
+
 
 def combination():
     global seqColors,lgSeq
@@ -50,26 +51,28 @@ def playSeq():
         
        
 def action(x):
-    global idCol,can_play,player_prop,lgSeq,lab_Message2
+    global idCol,can_play,player_prop,lgSeq,lbl_status,max_seq
     idCol=x
     if can_play:
         player_prop.append(x)
         if len(player_prop)==lgSeq:
             if check():
-                lab_Message2.configure(text="   SUCCESS = "+str(lgSeq))
+                lbl_status.configure(text="   SUCCESS = "+str(lgSeq))
                 lgSeq+=1
             else:
-                lab_Message2.configure(text="   GAME OVER ! MAX= "+str(lgSeq-1))
+                max_seq=lgSeq-1
+                lbl_status.configure(text="   GAME OVER ! MAX= "+str(max_seq))
                 lgSeq=1
+                check_highScores()
             window.after(1000,reinit)
         
 
 def reinit():
-    global can_play,player_prop,lab_Message,lab_Message2
+    global can_play,player_prop,lbl_instructions,lbl_status
     can_play=False
     player_prop=[]
-    lab_Message.configure(text=messages[0])
-    lab_Message2.configure(text="   CURRENT NUMBER = "+str(lgSeq))
+    lbl_instructions.configure(text=instructions[0])
+    lbl_status.configure(text="   CURRENT NUMBER = "+str(lgSeq))
     
 
 def check():
@@ -80,13 +83,13 @@ def check():
 
 
 def game():
-    global lgSeq,success,can_play,lab_Message
+    global lgSeq,success,can_play,lbl_instructions
     if not can_play:
-        lab_Message.configure(text=messages[1])
+        lbl_instructions.configure(text=instructions[1])
         combination()
         playSeq()
         can_play=True
-        lab_Message.configure(text=messages[2])
+        lbl_instructions.configure(text=instructions[2])
         
 
 def newGame():
@@ -96,16 +99,74 @@ def newGame():
 
 
 def printRules():
-    global ruleWindow
-    ruleWindow=tk.Tk()
+    ruleWindow=tk.Toplevel()
     ruleWindow.title("Simon rules")
-    frameRule=tk.Canvas(ruleWindow,bg='white',height=500,width=500)
-    frameRule.pack()  
     with open('rules_eng.txt') as f:
         gameRules=f.read()
-    lab_Rule=tk.Label(frameRule,text=gameRules,fg="black", anchor="e", justify=tk.LEFT)
-    lab_Rule.pack(side=tk.TOP)
+    lbl_rules=tk.Label(ruleWindow,text=gameRules,fg="black", anchor="e", justify=tk.LEFT)
+    lbl_rules.pack(side=tk.TOP)
     ruleWindow.mainloop()
+
+############################################################################
+# High score recovery
+############################################################################   
+def read_high_score():
+    '''
+    Loads the text document 'highScore.txt' used to recover the high score
+
+    Returns
+    -------
+    current_high_score : integer
+        current high score.
+
+    '''
+    with open('highScore.txt') as f:
+        score_recovery=f.readlines()
+    current_high_score=int(score_recovery[0].split()[-1])
+    return current_high_score
+
+def show_high_score():
+    global image_high_score
+    high_scoreWindow=tk.Toplevel()
+    high_scoreWindow.title("High score")
+    with open('highScore.txt') as f:
+        score_recovery=f.read()
+    lbl_image=tk.Label(master=high_scoreWindow,image=image_high_score)
+    lbl_image.pack()
+    lbl_score_recovery=tk.Label(high_scoreWindow,text=score_recovery,fg="black",font='Arial 22')
+    lbl_score_recovery.pack(side=tk.TOP)
+    high_scoreWindow.mainloop()  
+    
+def write_high_score():
+    global new_high_scoreWindow
+    playerName=ent_name.get()
+    with open('highScore.txt','w') as f:
+        f.write(playerName+'\t'+str(max_seq)+'\n')
+    new_high_scoreWindow.destroy()
+    window.after(1000,show_high_score)
+
+def new_high_score():
+    global ent_name,new_high_scoreWindow
+    new_high_scoreWindow=tk.Tk()
+    new_high_scoreWindow.title("New high score")
+    frameStat=tk.Canvas(new_high_scoreWindow,bg='white',height=500,width=500)
+    frameStat.pack()
+    ent_name=tk.Entry(frameStat)
+    ent_name.insert(0,'Player')
+    ent_name.pack(side=tk.BOTTOM)
+    lab_Stat=tk.Label(frameStat,text="Enter your name",fg="black")
+    lab_Stat.pack(side=tk.TOP)
+    but_Enter=tk.Button(frameStat,text="Enter",command=write_high_score)
+    but_Enter.pack(side=tk.BOTTOM)
+    new_high_scoreWindow.mainloop()
+    
+
+def check_highScores():
+    global highScore
+    if max_seq>highScore:
+        new_high_score()
+        
+highScore=read_high_score()
 #####################################################################################################
 ## Graphics interface
 #####################################################################################################
@@ -113,7 +174,7 @@ window=tk.Tk()
 window.title("Simon")
 frame=tk.Frame(window)
 frame.pack(side=tk.TOP)
-can = tk.Canvas(window)
+can = tk.Canvas(window,bg='black',height=60,width=50)
 can.pack(side=tk.TOP,padx=5,pady=5)
 top = tk.Menu(window)
 window.config(menu=top)
@@ -122,19 +183,23 @@ top.add_cascade(label='Game', menu=gameMenu)
 gameMenu.add_command(label='New game', command=newGame)
 gameMenu.add_command(label='Exit', command=window.destroy)
 top.add_command(label='Rules',command=printRules)
+top.add_command(label='Scores',command=show_high_score)
 
-lab_Message=tk.Label(frame,text=messages[0],fg="black")
-lab_Message.pack(side=tk.TOP)
+lbl_instructions=tk.Label(frame,text=instructions[0],fg="black")
+lbl_instructions.pack(side=tk.TOP)
 
 butGame=tk.Button(window,text="Play",command=game)
 butGame.pack(side=tk.LEFT)
-lab_Message2=tk.Label(window,text="   CURRENT NUMBER = "+str(lgSeq),fg="black")
-lab_Message2.pack(side=tk.LEFT)
+lbl_status=tk.Label(window,text="   CURRENT NUMBER = "+str(lgSeq),fg="black")
+lbl_status.pack(side=tk.LEFT)
 
 but_Colors=[]
 for count in range(len(colors)):
     color=colors[count]
     but_Colors.append(tk.Button(can,bg=color,fg="black",height = 15,width = 20,command=lambda x=count:action(x)))
     but_Colors[count].pack(side=tk.LEFT)
+
+name='highScore.gif'
+image_high_score=tk.PhotoImage(file=name)
 
 window.mainloop()
